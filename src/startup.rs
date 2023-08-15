e7db7d86ac5118587f7f30d3be0d1a7dccd7fda9
TomasLucas <tomaslucas@gmail.com>
2023-08-15 19:57:35 +0200

    Addapt test, configuration to use PgPool conections and individual database for testing

--- src/startup.rs
+++ src/startup.rs
@@ -1,17 +1,17 @@
+use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgPool;
 use std::net::TcpListener;
 
-use crate::routes::{health_check, subscribe};
-
-// We need to mark `run` as public.
-// It is no longer a binary entrypoint, therefore we can mark it as async
-// without having to use any proc-macro incantation.
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    let db_pool = web::Data::new(db_pool);
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            // Get a pointer copy and attach it to the application state
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();
