7f1d8873e15dd496f605eb56044317fc249c3d88
Tomás Lucas <tomaslucas@gmail.com>
2023-08-23 20:18:50 +0200

    modify structure to use environment variables

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,7 +1,15 @@
-//! src/configurations.rs
-
+//! src/configuration.rs
 use secrecy::ExposeSecret;
 use secrecy::Secret;
+use serde_aux::field_attributes::deserialize_number_from_string;
+use sqlx::postgres::PgConnectOptions;
+
+#[derive(serde::Deserialize)]
+pub struct ApplicationSettings {
+    #[serde(deserialize_with = "deserialize_number_from_string")]
+    pub port: u16,
+    pub host: String,
+}
 
 #[derive(serde::Deserialize)]
 pub struct Settings {
@@ -10,40 +18,26 @@ pub struct Settings {
 }
 
 #[derive(serde::Deserialize)]
-pub struct ApplicationSettings {
-    pub port: u16,
-    pub host: String,
-}
-
-#[derive(serde::Deserialize)]
 pub struct DatabaseSettings {
     pub username: String,
     pub password: Secret<String>,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
     pub database_name: String,
 }
 
 impl DatabaseSettings {
-    pub fn connection_string(&self) -> Secret<String> {
-        Secret::new(format!(
-            "postgres://{}:{}@{}:{}/{}",
-            self.username,
-            self.password.expose_secret(),
-            self.host,
-            self.port,
-            self.database_name
-        ))
+    pub fn without_db(&self) -> PgConnectOptions {
+        PgConnectOptions::new()
+            .host(&self.host)
+            .username(&self.username)
+            .password(&self.password.expose_secret())
+            .port(self.port)
     }
 
-    pub fn connection_string_without_db(&self) -> Secret<String> {
-        Secret::new(format!(
-            "postgres://{}:{}@{}:{}",
-            self.username,
-            self.password.expose_secret(),
-            self.host,
-            self.port
-        ))
+    pub fn with_db(&self) -> PgConnectOptions {
+        self.without_db().database(&self.database_name)
     }
 }
 
@@ -65,6 +59,11 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
         .add_source(config::File::from(
             configuration_directory.join(environment_filename),
         ))
+        .add_source(
+            config::Environment::with_prefix("APP")
+                .prefix_separator("_")
+                .separator("__"),
+        )
         .build()?;
     // Try to convert the configuration values ti read into
     // our Settings type
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,6 @@
 //! tests/health_check.rs
 use once_cell::sync::Lazy;
-use secrecy::ExposeSecret;
+// use secrecy::ExposeSecret;
 use sqlx::{postgres::PgPool, Connection, Executor, PgConnection};
 use std::net::TcpListener;
 use uuid::Uuid;
@@ -55,17 +55,16 @@ async fn spawn_app() -> TestApp {
 
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
     // Create database
-    let mut connection =
-        PgConnection::connect(&config.connection_string_without_db().expose_secret())
-            .await
-            .expect("Failed to connect to Postgres.");
+    let mut connection = PgConnection::connect_with(&config.without_db())
+        .await
+        .expect("Failed to connect to Postgres.");
     connection
         .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
         .await
         .expect("Faile to create database.");
 
     // Migrate database
-    let connection_pool = PgPool::connect(&config.connection_string().expose_secret())
+    let connection_pool = PgPool::connect_with(config.with_db())
         .await
         .expect("Faile to connect the Postgres.");
     sqlx::migrate!("./migrations")
