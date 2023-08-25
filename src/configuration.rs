5b4c6cfa20d3bbaa664c8f87d778cf62f64f02ec
Tomás Lucas <tomaslucas@gmail.com>
2023-08-25 17:20:34 +0200

    fix vulnerability for sqlx-tokio-rustls: https://discord.com/channels/864226046189764608/864226778737541131/1143868124777697371

--- src/configuration.rs
+++ src/configuration.rs
@@ -2,7 +2,7 @@
 use secrecy::ExposeSecret;
 use secrecy::Secret;
 use serde_aux::field_attributes::deserialize_number_from_string;
-use sqlx::postgres::PgConnectOptions;
+use sqlx::{postgres::PgConnectOptions, ConnectOptions};
 
 #[derive(serde::Deserialize)]
 pub struct ApplicationSettings {
@@ -37,7 +37,8 @@ impl DatabaseSettings {
     }
 
     pub fn with_db(&self) -> PgConnectOptions {
-        self.without_db().database(&self.database_name)
+        let options = self.without_db().database(&self.database_name);
+        options.log_statements(tracing_log::log::LevelFilter::Trace)
     }
 }
 
